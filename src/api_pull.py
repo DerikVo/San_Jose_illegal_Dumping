72edbcbe25c3b8cc2d5e0f2796a3bbc6f4345046
Derik Vo <DerikVoSanJose@Gmail.com>
2024-02-08 19:53:33 -0800

    updated commits and fixed filepath ways for csv, orginally checked for py file instead of csv file

--- src/api_pull.py
+++ src/api_pull.py
@@ -1,9 +1,12 @@
+"""
+Lasted updated
+2024-02-08
+"""
 import os
 import pandas as pd
 import requests
 from datetime import datetime, timezone, timedelta
 
-file_path = '../src/api_pull.py'
 """
 Code was asited by chatGPT; prompt focused on converted unix time into pst. Line 13 was fixed to convert to milliseconds and 21 was generated  to convert to PST
 
@@ -11,7 +14,7 @@ Then I converted those lines into a function to convert columns as needed.
 Since this is in San Jose I assume the timezone was in PST
 """
 def convert_time(col):
-    # Assuming df is your DataFrame and DATETIMERECEIVEDUTC is the column with Unix time in milliseconds
+    # Assuming the columns with Unix time are in milliseconds; dates make more sense with this method
     df[f'{col}'] = pd.to_datetime(df[f'{col}'] / 1000, unit='s')
 
     # Assuming your original time is in UTC
@@ -22,17 +25,19 @@ def convert_time(col):
     df[f'{col}'].dt.tz_localize(utc_timezone).dt.tz_convert(pst_timezone)
     return df
     # Now df[f'col'] contains the datetime with PST timezone
+"""
+This script is used to create an initial file for records on illegal dumping in the city of San Jose, California. If a file exist then it simplyy loads the CSV. The future goal is to identify how to create a filtered query to update new records. Currently I need to examine how to pull records past the newest date from whats in the datebase.
 
 
-if os.path.exists(file_path):
+"""
+file_path = '../data/illegal_dumping.csv'
+if not os.path.exists(file_path):
     url = 'https://geo.sanjoseca.gov/server/rest/services/OPN/OPN_OpenDataService/MapServer/458/query?where=1%3D1&outFields=*&outSR=4326&f=json'
     req = requests.get(url)
-    #orginally tested json file parasing
-    #sj = req.json()
-    req.status_code
+    sj = req.json()
     result_entries = []
 
-    for response in req.json()['features']:
+    for response in sj['features']:
         # Create a DataFrame with the first column and exclude the last two rows
         entry = pd.DataFrame(response).iloc[:-2, :1].transpose()
 
