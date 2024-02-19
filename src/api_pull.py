1804c123a9515c890948129ec0134c213721b61d
Derik Vo <DerikVoSanJose@Gmail.com>
2024-02-19 11:43:37 -0800

    seperated api query and cleaning to seperate scripts. Tested api query filters

--- src/api_pull.py
+++ src/api_pull.py
@@ -1,35 +1,17 @@
 """
-Lasted updated
-2024-02-08
-"""
-import os
-import pandas as pd
-import requests
-from datetime import datetime, timezone, timedelta
-
-"""
+20240219
 Code was asited by chatGPT; prompt focused on converted unix time into pst. Line 13 was fixed to convert to milliseconds and 21 was generated  to convert to PST
 
 Then I converted those lines into a function to convert columns as needed.
 Since this is in San Jose I assume the timezone was in PST
+
+This has gone through serval interations due to issues with importing data into postgresql
+
+
+Code was later updated to have a seperate script for pulling the data an cleaning the data
 """
-def convert_time(col):
-    # Assuming the columns with Unix time are in milliseconds; dates make more sense with this method
-    df[f'{col}'] = pd.to_datetime(df[f'{col}'] / 1000, unit='s')
-
-    # Assuming your original time is in UTC
-    utc_timezone = timezone.utc
-    pst_timezone = timezone(timedelta(hours=-8))  # Pacific Standard Time (PST)
-
-    # Convert UTC time to PST
-    df[f'{col}'].dt.tz_localize(utc_timezone).dt.tz_convert(pst_timezone)
-    return df
-    # Now df[f'col'] contains the datetime with PST timezone
-"""
-This script is used to create an initial file for records on illegal dumping in the city of San Jose, California. If a file exist then it simplyy loads the CSV. The future goal is to identify how to create a filtered query to update new records. Currently I need to examine how to pull records past the newest date from whats in the datebase.
 
 
-"""
 file_path = '../data/illegal_dumping.csv'
 if not os.path.exists(file_path):
     url = 'https://geo.sanjoseca.gov/server/rest/services/OPN/OPN_OpenDataService/MapServer/458/query?where=1%3D1&outFields=*&outSR=4326&f=json'
@@ -49,12 +31,6 @@ if not os.path.exists(file_path):
 
     # Reset the index of the final result_df
     df.reset_index(drop=True, inplace=True)
-    #list columns of fields that should be dates
-    cols = ['DATETIMERECEIVEDUTC', 'DATETIMECOMPLETEDUTC', 'DATETIMERECEIVED', 'LASTUPDATETHIRTYDAYS', 'LASTUPDATE']    
-    #loop through list od columns
-    for i in cols:
-        #uses function to convert to time into a human readable format
-        convert_time(i)
     #saves csv
     df.to_csv('../data/illegal_dumping.csv', index= False)
     #print a status code for creating a csv if it didnt exist before
@@ -62,4 +38,16 @@ if not os.path.exists(file_path):
 #if the file exist then it loads it as a csv.
 else:
     df= pd.read_csv('../data/illegal_dumping.csv')
-    print('illegal dumping csv loaded')
+    # Need to learn how to use the params with this datebase; using time and the column name is not working
+    # url = 'https://geo.sanjoseca.gov/server/rest/services/OPN/OPN_OpenDataService/MapServer/458/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+    # newest_date = df['DATETIMERECEIVEDUTC'].max()
+    # params = {'time' : newest_date+1}
+    # req = requests.get(url, params=params)
+    # sj = req.json()
+    # result_entries = []
+    # for response in sj['features']:
+    #     # Create a DataFrame with the first column and exclude the last two rows
+    #     entry = pd.DataFrame(response).iloc[:-2, :1].transpose()
+    #     result_entries.append(entry)
+    # df = pd.concat([df]+result_entries, ignore_index=True)
+    print('illegal dumping csv loaded and updated')
---
+++ src/database_insert.py
@@ -0,0 +1,38 @@
+"""
+Last updated: 20240219
+
+This script is a work around for inserting data into the database with individual insert statements. Currently have isues with using the import function and date's being out of range. Even when converting them to seconds or using date strings via api protocol. 
+
+"""
+
+
+def generate_insert_sql(df, table_name, output_file):
+    df.replace(np.nan, 0, inplace=True) #temporary fix to get pipeline to work; will need to fix later
+    # Open the SQL file we will use to populate the datebase
+    with open(output_file, 'w') as sql_file:
+        # Iterate through the DataFrame rows
+        for index, row in df.iterrows():
+            #get rid of quotes so the program doesnt break
+            row = row.apply(lambda x: str(x).replace("'", "''").replace('"', '""'))
+            #drops nan strings
+
+            # Construct the SQL insert statement for each row
+            sql = f"INSERT INTO {table_name} (DATETIMERECEIVEDUTC, DATETIMECOMPLETEDUTC, WORKORDERREFNO, OBJECTID, FACILITYID, INTID, COUNCILDISTRICT, SERVICERECORDID, SERVICECATEGORY, DATETIMERECEIVED, STATUS, DATETIMECOMPLETED, DETAILSUMMARY, LATITUDE, LONGITUDE, IMAGENAME, HYPERLINK, MYSANJOSEREFNO, LASTUPDATETHIRTYDAYS, LASTUPDATE, NOTES, ENTERPRISEID, GlobalID) VALUES "
+            sql += f"(to_timestamp('{row['DATETIMERECEIVEDUTC']}'), to_timestamp('{row['DATETIMECOMPLETEDUTC']}'), '{row['WORKORDERREFNO']}', '{row['OBJECTID']}', '{row['FACILITYID']}', '{row['INTID']}', '{row['COUNCILDISTRICT']}', {row['SERVICERECORDID']}, '{row['SERVICECATEGORY']}', to_timestamp('{row['DATETIMERECEIVED']}'), '{row['STATUS']}', to_timestamp('{row['DATETIMECOMPLETED']}'), '{row['DETAILSUMMARY']}', '{row['LATITUDE']}', '{row['LONGITUDE']}', '{row['IMAGENAME']}', '{row['HYPERLINK']}', '{row['MYSANJOSEREFNO']}', to_timestamp('{row['LASTUPDATETHIRTYDAYS']}'), to_timestamp('{row['LASTUPDATE']}'), '{row['NOTES']}', '{row['ENTERPRISEID']}', '{row['GlobalID']}');\n"
+            
+            # Write the SQL statement to the file
+            sql_file.write(sql)
+
+    print(f"SQL statements have been written to {output_file}")
+
+# specifiy the file we will be using
+df = pd.read_csv('../data/illegal_dumping.csv')
+
+# Specify the table name were inserting into
+table_name = 'illegal_dumping'
+
+# Specify the output SQL file name
+output_sql_file = '../src/illegal_dumping_data.sql'
+
+# Call the function
+generate_insert_sql(df, table_name, output_sql_file)
